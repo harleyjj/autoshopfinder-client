9e572388d09e528cf1c46f36d899423f2cb3e231
harleyjj <harleyyesm@gmail.com>
2018-07-06 07:06:07 -0700

    slight tweak

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,7 +12,7 @@ export function RegistrationPage(props) {
     }
     return (
         <div className="home">
-            <h2>Register for Foo App</h2>
+            <h2>Register for Autoshops.com</h2>
             <RegistrationForm />
             <Link to="/shops">Login</Link>
         </div>
